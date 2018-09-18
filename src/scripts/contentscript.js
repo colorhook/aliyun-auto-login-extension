cb95154f964bf2e19f639925eaaacf2956b6c482
liaojian <liaoj@nbugs.com>
2018-09-18 16:10:34 +0800

    add feature: 1> add sub login entry on main login panel. 2> detect logout action, avoid auto login after logout

--- src/scripts/contentscript.js
+++ src/scripts/contentscript.js
@@ -5,15 +5,37 @@ function el(id) {
   return document.getElementById(id)
 }
 
-storage.get('account', function(resp) {
-  var account = resp.account;
-  if (!account || !account.username || !account.password) {
-    return
-  }
-  if (!el('user_principal_name') || !el('J_FormNext')) {
-    return
-  }
-  el('user_principal_name').value = account.username
-  el('password_ims').value = account.password
-  document.querySelector('.login-form').submit()
-});
+if (location.href.indexOf("passport.aliyun.com") != -1) {
+    // 主账号入口
+    var links = document.querySelector('.login-links');
+    var redirectEl = document.createElement('a');
+    redirectEl.onclick = function() {
+      top.location = "https://signin.aliyun.com/login.htm";
+    }
+    redirectEl.innerText = "子账号登录"
+    redirectEl.style.cssText = "font-weight: 700;color: #f38; cursor: pointer"
+    links.appendChild(redirectEl)
+} else {
+  // 子账号入口
+  storage.get('account', function(resp) {
+    var account = resp.account;
+    if (!account || !account.username || !account.password) {
+      return
+    }
+    if (el('user_principal_name')) {
+      el('user_principal_name').value = account.username
+    }
+    if (el('password_ims')) {
+      el('password_ims').value = account.password
+    }
+
+    // 如果是登出操作，则不再自动登录
+    if (document.referrer.indexOf('passport.aliyun.com/logout.htm') != -1) {
+      return;
+    }
+    if (document.querySelector('.login-form')) {
+      document.querySelector('.login-form').submit()
+    }
+  });
+}
+
