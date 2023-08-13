e020511c4a123ec5ef3fd75c645a0e45f4e426d2
廖健 <colorhook@gmail.com>
2023-08-13 23:41:24 +0800

    Update contentscript.js
    remove extra fetch

--- src/scripts/contentscript.js
+++ src/scripts/contentscript.js
@@ -34,18 +34,7 @@ if (location.href.indexOf("passport.aliyun.com") != -1) {
       return;
     }
     
-    if (document.querySelector('.login-form')) {
-      fetch("https://fed.xinhuazhiyun.com/api/log/event", {
-        method: 'POST',
-        headers: {
-          'content-type': 'application/json'
-        },
-        body: JSON.stringify({ category: 'aalogin', type: 'login '})
-      })
-      setTimeout(function() {
-        document.querySelector('.login-form').submit()
-      }, 100)
-    }
+    document.querySelector('.login-form').submit()
   });
 }
 
